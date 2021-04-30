812af9c256f60b58c15947ad7511bb0917932459
marcovstiphout <marcovstiphout@gmail.com>
2021-04-30 16:21:41 +0200

    Created Initial Project

---
+++ src/Services/KweetService/Domain/Kweet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kwetter.Services.KweetService.Domain
+{
+    public class Kweet
+    {
+        public Guid Id { get; set; }
+
+        public string KweetMessage { get; set; }
+
+        public DateTime KweetPostDate { get; set; }
+    }
+}
---
+++ src/Services/KweetService/Rest/Controllers/KweetController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kwetter.Services.KweetService.Domain;
+
+namespace Kwetter.KweetService.Rest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KweetController : ControllerBase
+    {
+        private readonly ILogger<KweetController> _logger;
+
+        public KweetController(ILogger<KweetController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Kweet> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Kweet
+            {
+                Id = Guid.NewGuid(),
+                KweetMessage = "Message Test: " + rng.Next(-20, 55),
+                KweetPostDate = DateTime.Now.AddDays(index)
+            })
+            .ToArray();
+        }
+    }
+}
